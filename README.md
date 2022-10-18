dbf372fc68e4e1293afeb5b1da0aedc0a684785e
Anthony <quaz711@hotmail.com>
2022-10-17 20:53:09 -0400

    Finished javascript file

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Password Generator Starter Code
+# A webpage that meets accessibility standards
+
+A marketing agency wants a codebase that follows accessibility standards so that their site is optimized for search engines
+
+![This is a screenshot showing the top of the client's website.](./assets/images/Top.png)
+
+![This is a screenshot showing the middle of the client's website.](./assets/images/Middle.png)
+
+![This is a screenshot showing the bottom of the client's website.](./assets/images/Bottom.png)
+
+## View the source code; Update HTML to semantic elements
+
+* Upon first look of the HTML code there were no semantic elements. I removed div and some classes that were not needed to add semantic 
+elements such as header, section, article, aside, and footer.
+
+## View the structure of the HTML elements; Follows a logical structure independent of styling and positioning
+
+* Upon first look of the HTML and CSS code some indentation seemed out of order and lines ran off the coding screen so some organizing 
+and comments were made to make it clearer to read and comprehend.
+
+## View image elements; Accessible alt attributes
+
+* Upon first look of the HTML there were no alt attributes, so alt attributes were added describbing the images in the event they did not load.
+
+
+## View heading attributes; They fall in sequential order
+
+* Link to CSS file was put below the title declaration.
+
+## View title element; Find it has a concise and descriptive title
+
+* Changed the original title "website" to "Horiseon" to reflect the company.
+
+## Additional added items for accessibility
+
+* Added link to top of page to heart icon on bottom of page
+* Added link to top of page to Search Engine Optimization image
+* Added link to top of page to Online Reputation Management
+* Added link to top of page to Social Media Marketing
+
+# Links to deployed application
+
+## GitHub repository for Challenge One
+[Github Repo](https://github.com/Quaz711/Module-One.git)
+
+
+## Live website for Challenge One
+[Live Website](https://quaz711.github.io/Module-One/)
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,96 @@
 // Assignment code here
+var lowerCase;
+var upperCase;
+var numeric;
+var specialCharacters;
+var passwordLength;
+var possiblePassword = [];
+var generatedPassword;
 
+function generatePassword() {
+  generatedPassword = "";
+  //Asks user how long they want the password to be
+  passwordLength = window.prompt("Please enter length of password (8-128)", "8");
+
+  //Checks to make sure use entered correct parameters
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert("You chose: " + passwordLength);
+    questions();
+  }
+  //Checks to see if users wants to exit0
+  else if (!passwordLength) {
+    return;
+  }
+  //Error protection if user enters outside parameters or invalid characters and makes them re-enter or exit0
+  else {
+    window.alert("Invalid entry");
+    return generatePassword();
+  }
+}
+
+function questions (){
+  console.log("Entered Questions Function");
+  //Asks user if they want to include lowercase
+  lowerCase = window.confirm("Include lowercase?");
+  if (lowerCase){
+    //Adds lowercase alphabet to the array generatedPassword
+    possiblePassword.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+    window.alert("You chose to include lowercase");
+  }
+  else {
+    window.alert("You chose to NOT include lowercase");
+  }
+  //Asks user if they want to include uppercase
+  upperCase = window.confirm("Include UPPERCASE?");
+  if (upperCase){
+    //Adds uppercase alphabet to the array generatedPassword
+    possiblePassword.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+    window.alert("You chose to include UPPERCASE");
+  }
+  else {
+    window.alert("You chose to NOT include UPPERCASE");
+  }
+  //Asks user if they want to include numbers
+  numeric = window.confirm("Include numbers?");
+  if (numeric){
+    //Adds numbers to the array generatedPassword
+    possiblePassword.push("1", "2", "3", "4", "5", "6", "7", "8", "9", "0");
+    window.alert("You chose to include numbers");
+  }
+  else {
+    window.alert("You chose to NOT include numbers");
+  }
+  //Asks user if they want to include special characters
+  specialCharacters = window.confirm("Include special characters?");
+  if (specialCharacters){
+    //Adds uppercase alphabet to the array generatedPassword
+    possiblePassword.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "'", "|", ";", ":", ",", ".", "<", ">", "/", "?", "`", "~");
+    window.alert("You chose to include special characters");
+  }
+  else {
+    window.alert("You chose to NOT include special characters");
+  }
+  //Checks to make sure the user selected at least one criteria and continues to generate the code
+  if (lowerCase || upperCase || numeric || specialCharacters){
+    window.alert("GENERATING PASSWORD");
+    generateItAlready();
+  }
+  //If user did not select at least one criteria then it sends them baack through the questions
+  else {
+    window.alert("You must accept at least ONE criteria");
+    return questions();
+  }
+}
+
+function generateItAlready (){
+  for (var i = 0; i < passwordLength; i++)
+  {
+    //Generates random numbers according to all the selected criteria and pulls that criteria to create a random unique password
+    var index = Math.floor(Math.random() * possiblePassword.length);
+    //Takes array and turns it into a string while adding the new random password criteria
+    generatedPassword += possiblePassword[index];
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,10 +98,17 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  //takes generated code and passes it to password so it can be displayed
+  password = generatedPassword;
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  console.log("Button was pushed");
+  if (password != null) {
+    passwordText.value = password;
+  }
+  //Added if invalid entries pass error detections above
+  else {
+    passwordText.value = "Try again!";
+  }
 }
 
 // Add event listener to generate button
